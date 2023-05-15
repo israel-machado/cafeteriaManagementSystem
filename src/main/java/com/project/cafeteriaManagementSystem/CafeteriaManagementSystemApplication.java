29f34f59374bd00973697b7064fc5ffaa8e7f030
Israel Machado <israelmach26@gmail.com>
2023-05-15 10:01:55 -0300

    Project created

---
+++ src/main/java/com/project/cafeteriaManagementSystem/CafeteriaManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.project.cafeteriaManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeteriaManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeteriaManagementSystemApplication.class, args);
+	}
+
+}
